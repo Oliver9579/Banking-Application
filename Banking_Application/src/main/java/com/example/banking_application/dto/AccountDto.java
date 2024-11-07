e16bdd3119c294687246d812a183c77107dbba80
Oliver9579 <szabo.oliver2001@gmail.com>
2024-11-07 15:24:51 +0100

    Refactor AccountDto to use Java record
    Replace AccountDto class with a record to simplify the code and improve immutability. Updated AccountMapper accordingly to use record accessor methods instead of getters.

--- Banking_Application/src/main/java/com/example/banking_application/dto/AccountDto.java
+++ Banking_Application/src/main/java/com/example/banking_application/dto/AccountDto.java
@@ -1,14 +1,4 @@
 package com.example.banking_application.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class AccountDto {
-
-    private Long id;
-    private String accountHolderName;
-    private double balance;
-
+public record AccountDto(Long id, String accountHolderName, Double balance) {
 }
